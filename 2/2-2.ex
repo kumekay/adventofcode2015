092a3433344d9caa12e89114a312e8696c4fcdee
Sergei Silnov <po@kumekay.com>
2016-03-15 06:13:25 +0100

    add 2/2

---
+++ 2/2-2.ex
@@ -0,0 +1,14 @@
+defmodule Ribbon do
+  def length([ {l, _ } | [ {w, _ } | [ {h, _ } | _ ]]]) do
+    perimeters = [2*(l+w), 2*(w+h), 2*(h+l)]
+    l*w*h + Enum.min(perimeters)
+   end
+end
+
+len = File.stream!("2.input")
+|> Enum.map( fn(x) -> (String.split(x, "x") |> Enum.map(&Integer.parse/1) |> Ribbon.length) end)
+|> Enum.sum
+# len = String.split("1x1x10", "x") |> Enum.map(&Integer.parse/1) |> Ribbon.length
+
+
+IO.puts len
