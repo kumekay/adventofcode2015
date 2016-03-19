f1c58da0175b87b6014c85fb362f1b8fccd4308d
Sergei Silnov <po@kumekay.com>
2016-03-19 23:07:50 +0100

    add 10-1 and 10-2

---
+++ 10/part2.ex
@@ -0,0 +1,17 @@
+parse = fn
+    (x) -> x |>
+        String.replace("111", "eq") |>
+        String.replace("222", "ew") |>
+        String.replace("333", "ee") |>
+        String.replace("11", "wq") |>
+        String.replace("22", "ww") |>
+        String.replace("33", "we") |>
+        String.replace("1", "qq") |>
+        String.replace("2", "qw") |>
+        String.replace("3", "qe") |>
+        String.replace("q", "1") |>
+        String.replace("w", "2") |>
+        String.replace("e", "3")
+end
+
+File.read!("input.txt") |> Stream.iterate(parse) |> Enum.at(50) |> String.length |> IO.puts
